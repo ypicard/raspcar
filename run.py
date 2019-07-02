244c9241a8fe58efc25834a6285dff90aacf7064
Yann Picard <yann.picard95@gmail.com>
2019-07-02 10:57:04 +0200

    Simple rewar

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
 
 class Radar():
 
-    def __init__(self, car, radians_offset=0, length=50):
+    def __init__(self, car, radians_offset=0, length=80):
         self.car = car
         self._length = length
         self.radians_offset = radians_offset
@@ -31,6 +31,7 @@ class Radar():
         self.start_y = self.car.center_y
         self.end_x = self.car.center_x + cos(self.radians) * self._length
         self.end_y = self.car.center_y + sin(self.radians) * self._length
+        self.radians = random.random() * 3.14
 
     def update(self, terrain):
         self.radians = self.car.radians + self.radians_offset
@@ -62,7 +63,7 @@ class Car(arcade.Sprite):
     CAR_WIDTH = 50
     CAR_LENGTH = 100
     _speed = 5
-    _steer = 0.1
+    _steer = 0.2
 
     def __init__(self, terrain):
         super().__init__('images/car.png', scale=0.25, center_x=SCREEN_WIDTH/2, center_y=SCREEN_HEIGHT/2)
@@ -176,9 +177,9 @@ class MyGame(arcade.Window):
 
         # map 3
         # bottom
-        self._world.append([(0, 350), (SCREEN_WIDTH, 350), (SCREEN_WIDTH, 0), (0, 0)]) 
+        self._world.append([(0, 320), (SCREEN_WIDTH, 320), (SCREEN_WIDTH, 0), (0, 0)]) 
         # top
-        self._world.append([(0, SCREEN_HEIGHT), (SCREEN_WIDTH, SCREEN_HEIGHT), (SCREEN_WIDTH, SCREEN_HEIGHT - 350), (0, SCREEN_HEIGHT- 350)])
+        self._world.append([(0, SCREEN_HEIGHT), (SCREEN_WIDTH, SCREEN_HEIGHT), (SCREEN_WIDTH, SCREEN_HEIGHT - 320), (0, SCREEN_HEIGHT- 320)])
         
 
     def setup(self):
@@ -202,6 +203,7 @@ class MyGame(arcade.Window):
         for o in self._world:
             arcade.draw_polygon_filled(o, arcade.color.BLUE)
         arcade.draw_text(str(self._steps), 10, SCREEN_HEIGHT-20, arcade.color.WHITE)
+        arcade.draw_text(str(self.cum_reward), 10, SCREEN_HEIGHT-40, arcade.color.WHITE)
 
     def update(self, delta_time):
         """
@@ -233,7 +235,7 @@ class MyGame(arcade.Window):
         # get current state
         cur_state = self._get_state()
         print('cur state', cur_state)
-        cur_state = np.reshape(cur_state, [1, 4])
+        cur_state = np.reshape(cur_state, [1, STATE_SIZE])
 
         # get agent action
         action = agent.act(cur_state)
@@ -246,20 +248,19 @@ class MyGame(arcade.Window):
         self.car.update()
 
         next_state = self._get_state()
-        collides = self.car.collides
         print('next state', next_state)
-        reward = self._get_reward(next_state, collides)
-        done = collides or self._steps> 500 # collides
+        reward = self._get_reward()
+        done = self.car.collides or self._steps > 500  # collides
         print(f"reward={reward}")
         self.cum_reward += reward
-        next_state = np.reshape(next_state, [1, 4])
+        next_state = np.reshape(next_state, [1, STATE_SIZE])
 
         self.agent.remember(cur_state, action, reward, next_state, done)
 
         self._steps += 1
         # replay
-        if self._steps > 10 and self._steps % 10 == 0:
-            history = self.agent.replay(10)
+        if self._steps > 32 and self._steps % 32 == 0:
+            history = self.agent.replay(32)
             self.history['loss'].append(history.history['loss'][0])
 
         if done:
@@ -307,16 +308,24 @@ class MyGame(arcade.Window):
     def _get_state(self):
         car_state = self.car.get_state()
         # [change_angle, ...bips]
-        state = car_state[3:]
-        
+        is_turning = 1 if car_state[3] != 0 else 0
+        radar_values = car_state[4:]
+        # try removing is turning from state, but keep it in reward computation
+        # state = [is_turning] + radar_values
+        state = radar_values
         return state
 
-    def _get_reward(self, state, collides):
-        if collides:
+    def _get_reward(self):
+        if self.car.collides:
             # collides
             return -500
         # max reward - radars activated - turning angle (to reward straight lines)
-        return 4 - sum(state[1:]) - abs(state[0]) * 3
+        reward = 1
+        is_turning = 1 if self.car.change_angle != 0 else 0
+        reward -= is_turning
+        # reward -= (sum((state[1:]) +state[0])/len(state(1:))
+
+        return  reward
 
 
 class DQNAgent():
@@ -325,10 +334,10 @@ class DQNAgent():
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
         self.gamma = 0.95  # discount rate
-        self.epsilon = 0.05  # exploration rate
-        self.epsilon_min = 0.05
+        self.epsilon = 0.5  # exploration rate
+        self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
-        self.learning_rate = 0.001
+        self.learning_rate = 0.005
         self.model = self._build_model()
 
     def _build_model(self):
@@ -374,10 +383,10 @@ class DQNAgent():
         return history
 
 
+STATE_SIZE = 3
+ACTION_SIZE = 3
 if __name__ == "__main__":
-    state_size = 4
-    nb_actions = 3
-    agent = DQNAgent(state_size, nb_actions)
+    agent = DQNAgent(STATE_SIZE, ACTION_SIZE)
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, agent=agent)
     game.setup()
     arcade.run()

f7e2d3dadec3106d0c13e58d57c0e7820a4850b9
Yann Picard <yann.picard95@gmail.com>
2019-07-02 18:36:09 +0200

    stack frames

--- run_gym.py
+++ run_gym.py
@@ -16,7 +16,7 @@ class DQNAgent():
         self.epsilon = epsilon  # exploration rate
         self.epsilon_min = 0.05
         self.epsilon_decay = 0.995
-        self.learning_rate = 0.1
+        self.learning_rate = 0.01
         self.model = self._build_model()
 
     def _build_model(self):

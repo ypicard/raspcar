26947969b25774bbb96255bfbb551cce2adc21a9
Yann Picard <yann.picard95@gmail.com>
2019-07-11 12:15:00 +0200

    Pre aiohttp

--- socketio/server.py
+++ socketio/server.py
@@ -3,15 +3,15 @@ from collections import deque
 from flask import Flask, Response, render_template
 from camera_socket import CameraSocket
 from radar_socket import RadarSocket
-from flask_socketio import SocketIO, send, emit
+import socketio
 from flask_cors import CORS
 from time import sleep
 import threading
 
+sio = socketio.Server(async_mode='threading')
 app = Flask(__name__)
 CORS(app)
-socketio = SocketIO(app)
-
+app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
 
 @app.route("/")
 def home():
@@ -33,22 +33,30 @@ def camera_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-@socketio.on('connect', namespace="/radar")
-def radar_socket_connected():
-    print("SHOULD GO IN")
-    def get_value():
-        print("MDR")
-        while True:
-            value = radar_socket.get_value()
-            if not value:
-                continue
-            print('should emit')
-            emit('new value', value)
 
-    print("RADAR CONNECTED")
-    thread = threading.Thread(target = get_value)
-    thread.start()
-    emit('lol')
+@sio.event( namespace="/radar")
+def connect(sid, environ):
+    print('radar socket connected ', sid)
+    sio.emit("new value", "ok", namespace="/radar")
+
+
+
+# @socketio.on('connect', namespace="/radar")
+# def radar_socket_connected():
+#     print("SHOULD GO IN")
+#     def get_value():
+#         print("MDR")
+#         while True:
+#             value = radar_socket.get_value()
+#             if not value:
+#                 continue
+#             print('should emit')
+#             emit('new value', value)
+
+#     print("RADAR CONNECTED")
+#     thread = threading.Thread(target = get_value)
+#     thread.start()
+#     emit('lol')
     
 
 
@@ -59,4 +67,4 @@ if __name__ == "__main__":
     camera_socket = CameraSocket(host='localhost', port=8089, frame_size=2730)
     radar_socket = RadarSocket(host='localhost', port=8090)
     logging.info("Starting Flask server")
-    socketio.run(app)
+    app.run(threaded=True)

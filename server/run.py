66449568aca7681d19168d71f145f2f57385b13d
Yann Picard <yann.picard95@gmail.com>
2019-07-12 17:40:54 +0200

    Remove socketio logs

--- server/run.py
+++ server/run.py
@@ -2,7 +2,8 @@
 import sys
 import logging
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
+logging.getLogger('socketio').setLevel(logging.ERROR)
+logging.getLogger('engineio').setLevel(logging.ERROR)
 import server
 
 if __name__ == '__main__':
